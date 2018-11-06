00317e5f13a1061aa514213afdfffaa8c02bd4ae
Fox <fox@foxdev.io>
2018-11-06 16:08:25 +0100

    [Web] Now toggles toggle the log visibility

--- web/components/Screens.tsx
+++ web/components/Screens.tsx
@@ -20,17 +20,23 @@ export default class Screens extends Component<any, any> {
     @computed get logs() {
         return this.props.ElasticFox.logs;
     }
+    @computed get checkedMappings() {
+        return this.props.ElasticFox.checkedMappings;
+    }
     public render() {
-        const nodes = this.logs.map((log: ILog) => {
-            return (<LogLine
-                harvester={log.harvester}
-                harvesterColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}`)}
-                application={log.application}
-                applicationColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}|${log.application}`)}
-                file={log.file}
-                fileColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}|${log.application}|${log.file}`)}
-            >{log.line}</LogLine>);
-        });
+        const nodes = this.logs.reduce((filtered: JSX.Element[], log: ILog) => {
+            if (this.checkedMappings.get(`${log.harvester}|${log.application}|${log.file}`)[0]) {
+                filtered.push(<LogLine
+                    harvester={log.harvester}
+                    harvesterColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}`)}
+                    application={log.application}
+                    applicationColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}|${log.application}`)}
+                    file={log.file}
+                    fileColor={(this.props.ElasticFox as Client).colorMapings.get(`${log.harvester}|${log.application}|${log.file}`)}
+                >{log.line}</LogLine>);
+            }
+            return filtered;
+        }, []);
         return (
             <div class={classMain}>
                 <Screen>
